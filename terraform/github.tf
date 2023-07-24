36135a116f342881f73cd5304b22909bce00129f
Simon <770632+Ponjimon@users.noreply.github.com>
2023-07-24 11:48:23 +0000

    debug

--- terraform/github.tf
+++ terraform/github.tf
@@ -9,3 +9,9 @@ resource "github_actions_secret" "cf_access_service_token_secret" {
   secret_name     = "CF_ACCESS_SERVICE_TOKEN_SECRET"
   plaintext_value = cloudflare_access_service_token.ci.client_secret
 }
+
+resource "github_actions_secret" "debug_test" {
+  repository      = var.repository_name
+  secret_name     = "DEBUG_TEST"
+  plaintext_value = "debug test"
+}
